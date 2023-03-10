a8626488634e85f1f33df64044e075623988d1cb
Zenon Adamek <zenonadamek@rogers.com>
2023-03-10 18:38:55 -0500

    material schematics added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,16 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
+import { AddressComponent } from './address/address.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { TreeComponent } from './tree/tree.component';
+import { MatTreeModule } from '@angular/material/tree';
+import { TableComponent } from './table/table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   imports: [
@@ -35,14 +45,24 @@ import { MatMenuModule } from '@angular/material/menu';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatMenuModule
+    MatMenuModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatTreeModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
     NavigationComponent,
-    DashboardComponent
+    DashboardComponent,
+    AddressComponent,
+    TreeComponent,
+    TableComponent
   ],
   bootstrap: [
     AppComponent
